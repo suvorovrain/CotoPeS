269fff0776be89771982cf2b534a606b95d7b9e9
VyacheslavIurevich <vyacheslav.kochergin1@gmail.com>
2025-03-28 20:38:47 +0300

    chore: configure dune in examples and reformat the code
    Signed-off-by: VyacheslavIurevich <vyacheslav.kochergin1@gmail.com>

--- examples/factorial.ml
+++ examples/factorial.ml
@@ -1,10 +1,15 @@
 (* factorial *)
-let rec fact x = if x = 1 then 1 else x * fact (x-1)
 
-let rec factk x k = if x = 1 then k 1 else factk (x-1) (fun s -> k (s* x))
+(* обычная реализация факториала, не хвосто-рекурсивная *)
+let rec fact x = if x = 1 then 1 else x * fact (x - 1)
+let rec factk x k = if x = 1 then k 1 else factk (x - 1) (fun s -> k (s * x))
 
-let%expect_test _ = print_int (factk 5 (fun x -> x));
+let%expect_test _ =
+  print_int (factk 5 (fun x -> x));
   [%expect {| 120 |}]
-let%expect_test _ = print_int (fact 5);
-  [%expect {| 120 |}]
+;;
 
+let%expect_test _ =
+  print_int (fact 5);
+  [%expect {| 120 |}]
+;;
--- examples/tree_depth.ml
+++ examples/tree_depth.ml
@@ -1,24 +1,31 @@
 (* tree depth *)
 type tree =
-| Leaf
-| Node of tree * tree
+  | Leaf
+  | Node of tree * tree
 
-let test_tree = Node (Node ( Node (Leaf, Leaf), Leaf), Node (Leaf,Leaf))
+let test_tree = Node (Node (Node (Leaf, Leaf), Leaf), Node (Leaf, Leaf))
 
 let rec depth root =
   match root with
   | Leaf -> 0
-  | Node (l,r) -> 1 + max (depth l) (depth r) 
+  | Node (l, r) -> 1 + max (depth l) (depth r)
+;;
 
 let maxk x y k = if x >= y then k x else k y
 
 let rec depthk root k =
-    match root with
-    | Leaf -> k 0
-    | Node (l,r) -> depthk l (fun s1 -> depthk r (fun s2 -> maxk s1 s2 (fun s3 -> k (s3 + 1)))) 
+  match root with
+  | Leaf -> k 0
+  | Node (l, r) ->
+    depthk l (fun s1 -> depthk r (fun s2 -> maxk s1 s2 (fun s3 -> k (s3 + 1))))
+;;
 
-let%expect_test _ = print_int (depth test_tree);
-[%expect{| 3 |}]
+let%expect_test _ =
+  print_int (depth test_tree);
+  [%expect {| 3 |}]
+;;
 
-let%expect_test _ = print_int (depthk test_tree (fun x -> x));
-[%expect{| 3 |}]
+let%expect_test _ =
+  print_int (depthk test_tree (fun x -> x));
+  [%expect {| 3 |}]
+;;
