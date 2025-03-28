a17d28aee113cb9447650dab3771ba37f07bfe35
Vyacheslav Kochergin <141725581+VyacheslavIurevich@users.noreply.github.com>
2025-03-28 20:48:42 +0300

    Я всё переделал а ты теперь проверяй (#1)
    * chore: update docs
    
    Signed-off-by: VyacheslavIurevich <vyacheslav.kochergin1@gmail.com>
    
    * chore: add ocamlformat config
    
    Signed-off-by: VyacheslavIurevich <vyacheslav.kochergin1@gmail.com>
    
    * chore: configure build
    
    Signed-off-by: VyacheslavIurevich <vyacheslav.kochergin1@gmail.com>
    
    * chore: configure dune in examples and reformat the code
    
    Signed-off-by: VyacheslavIurevich <vyacheslav.kochergin1@gmail.com>
    
    * refactor: rework examples as i like
    
    Signed-off-by: VyacheslavIurevich <vyacheslav.kochergin1@gmail.com>
    
    ---------
    
    Signed-off-by: VyacheslavIurevich <vyacheslav.kochergin1@gmail.com>

--- examples/tree_size.ml
+++ examples/tree_size.ml
@@ -1,26 +1,32 @@
-(* tree size *)
+(* tree size - количество узлов в бинарном дереве *)
 type tree =
-| Leaf
-| Node of tree * tree
+  | Leaf
+  | Node of tree * tree
 
-let test_tree = Node (Node ( Node (Leaf, Leaf), Leaf), Node (Leaf,Leaf))
-
+(* обычный поиск размера дерева не в хвостовом стиле *)
 let size root =
   let rec helper = function
-  | Leaf -> 0
-  | Node (l,r) -> 1 + helper l + helper r
-in helper root
+    | Leaf -> 0
+    | Node (l, r) -> 1 + helper l + helper r
+  in
+  helper root
+;;
 
-let rec sizek root k = 
-match root with
-| Leaf -> k 0
-| Node(l,r) -> sizek l (fun s1 -> sizek r (fun s2 -> k (s1 + s2 + 1)))
- 
+(* поиск размера дерева в CPS-стиле *)
+let rec sizek root k =
+  match root with
+  | Leaf -> k 0
+  | Node (l, r) -> sizek l (fun s1 -> sizek r (fun s2 -> k (s1 + s2 + 1)))
+;;
 
-let%expect_test _ = 
+let test_tree = Node (Node (Node (Leaf, Leaf), Leaf), Node (Leaf, Leaf))
+
+let%expect_test "Simple size evaluating" =
   print_endline (string_of_int (size test_tree));
   [%expect {| 4 |}]
+;;
 
-let%expect_test _ = 
-  print_endline (string_of_int (sizek test_tree (fun x -> x)));
+let%expect_test "CPS size evaluating" =
+  print_endline (string_of_int (sizek test_tree Fun.id));
   [%expect {| 4 |}]
+;;
