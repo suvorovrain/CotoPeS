d5f82ea94fe9ee59998b93075791cf8232578fa6
VyacheslavIurevich <vyacheslav.kochergin1@gmail.com>
2025-03-28 20:43:22 +0300

    refactor: rework examples as i like
    Signed-off-by: VyacheslavIurevich <vyacheslav.kochergin1@gmail.com>

--- examples/tree_depth.ml
+++ examples/tree_depth.ml
@@ -1,9 +1,9 @@
-(* tree depth *)
+(* tree depth - вычисление глубины бинарного дерева *)
 type tree =
   | Leaf
   | Node of tree * tree
 
-let test_tree = Node (Node (Node (Leaf, Leaf), Leaf), Node (Leaf, Leaf))
+(* обычная реализация не в хвостовом стиле *)
 
 let rec depth root =
   match root with
@@ -11,7 +11,11 @@ let rec depth root =
   | Node (l, r) -> 1 + max (depth l) (depth r)
 ;;
 
+(* поиск наибольшего из двух чисел в CPS-стиле *)
+
 let maxk x y k = if x >= y then k x else k y
+
+(* поиск глубины в CPS-стиле *)
 
 let rec depthk root k =
   match root with
@@ -20,12 +24,14 @@ let rec depthk root k =
     depthk l (fun s1 -> depthk r (fun s2 -> maxk s1 s2 (fun s3 -> k (s3 + 1))))
 ;;
 
-let%expect_test _ =
+let test_tree = Node (Node (Node (Leaf, Leaf), Leaf), Node (Leaf, Leaf))
+
+let%expect_test "Simple depth evaluating" =
   print_int (depth test_tree);
   [%expect {| 3 |}]
 ;;
 
-let%expect_test _ =
-  print_int (depthk test_tree (fun x -> x));
+let%expect_test "CPS depth evaluating" =
+  print_int (depthk test_tree Fun.id);
   [%expect {| 3 |}]
 ;;
