37cad7f60558d0aef028a5d4abe8a9030d7aa594
Rodion Suvorov <rodion.suvorov.94@mail.ru>
2025-03-28 22:06:13 +0300

    feat: CPS fibonacci

---
+++ examples/fibonacci.ml
@@ -0,0 +1,12 @@
+(*обычный фибоначи*)
+let rec fib n = if n <= 1 then 1 else fib(n-1) + fib(n-2)
+
+(* CPS (мега хайп) fibonacci*)
+
+let rec fibk n k = if n <= 1 then k 1 else fibk (n-1) (fun s1 -> fibk (n-2) (fun s2 -> k (s1 + s2))) 
+
+let%expect_test "Common Fibonacci" = print_int (fib 5);
+[%expect{| 8 |}]
+
+let%expect_test "Sigma Fibonacci" = print_int (fibk 30 Fun.id);
+[%expect{| 1346269 |}]
