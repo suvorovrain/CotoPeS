bcde93741753f9ebc07d63b610fd8133d4048483
Homka122 <kombaev02@gmail.com>
2025-03-31 04:58:47 +0300

    Feat: add tests for stack overflow showing

--- src/map_exam2025_1.ml
+++ src/map_exam2025_1.ml
@@ -5,6 +5,15 @@ let print_list ~print_element list =
   List.iter (fun x -> print_element x; print_string " ") list;
   print_newline ()
 
+let print_list_n ~print_element n list =
+  let rec take n = function
+    | [] -> []
+    | _ when n <= 0 -> []
+    | x :: xs -> x :: take (n - 1) xs in
+  List.iter (fun x -> print_element x; print_string " ") (take n list);
+  print_newline ()
+;;
+
 (* обычная мапа *)
 let rec map f xs =
   match xs with
@@ -30,3 +39,16 @@ let%expect_test "default map" = print_list ~print_element:print_int (map (fun x 
 let%expect_test "CPS map" = print_list ~print_element:print_int (mapk ( (fun x -> factk x)) [2;4;6]);
 [%expect{| 2 24 720 |}]
 
+let huge_list = List.init 1_000_000 (fun _ -> 1)
+
+let%expect_test "default map huge list" = 
+  (try
+    print_list_n ~print_element:print_int 5 (map (fun x -> fact x) huge_list)
+  with | Stack_overflow -> print_endline "Stack overflow!");
+  [%expect {| Stack overflow! |}]
+
+let%expect_test "CPS map huge list" = 
+  (try
+    print_list_n ~print_element:print_int 5 (mapk (fun x -> factk x) huge_list)
+  with | Stack_overflow -> print_endline "Stack overflow!");
+  [%expect {| 1 1 1 1 1 |}]
