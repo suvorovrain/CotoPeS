912513d8c3a48ff47ab53f6e602a345e18983294
Homka122 <kombaev02@gmail.com>
2025-03-31 19:04:18 +0300

    Docs: initial commit

---
+++ docs/astro.config.mjs
@@ -0,0 +1,22 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+
+// https://astro.build/config
+export default defineConfig({
+    // site: "https://suvorovrain.github.io",
+    // base: "CotoPeS",
+    integrations: [
+        starlight({
+            title: "CotoPeS",
+            social: {
+                github: "https://github.com/suvorovrain/CotoPeS",
+            },
+            sidebar: [
+                {
+                    slug: "cps",
+                },
+            ],
+        }),
+    ],
+});
