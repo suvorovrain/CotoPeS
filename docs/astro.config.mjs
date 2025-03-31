6627a43b24f6c1a62db42ec39a9f6e68ae832122
Homka122 <kombaev02@gmail.com>
2025-03-31 20:01:48 +0300

    Fix: add base to astro config

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -5,7 +5,7 @@ import starlight from "@astrojs/starlight";
 // https://astro.build/config
 export default defineConfig({
     // site: "https://suvorovrain.github.io",
-    // base: "CotoPeS",
+    base: "CotoPeS",
     integrations: [
         starlight({
             title: "CotoPeS",
