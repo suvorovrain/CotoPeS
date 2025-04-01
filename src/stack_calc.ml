465fc1e22f7e88cf9b6d9f699681a902fb2f39fe
VyacheslavIurevich <vyacheslav.kochergin1@gmail.com>
2025-04-01 12:25:30 +0300

    test: check stack calc on big expr
    Signed-off-by: VyacheslavIurevich <vyacheslav.kochergin1@gmail.com>

--- src/stack_calc.ml
+++ src/stack_calc.ml
@@ -32,3 +32,21 @@ let%expect_test "Eval expression" =
   print_int (start push 1 push 2 add push 3 mul fin);
   [%expect {| 9 |}]
 ;;
+
+let sum_n_ones n =
+  let rec build_ops n ops =
+    if n <= 0 then start ops else build_ops (n - 1) (fun stack -> push stack 1 ops)
+  in
+  let rec add_ops count ops =
+    if count <= 0 then ops else add_ops (count - 1) (fun stack -> add stack ops)
+  in
+  build_ops n (add_ops (n - 1) fin)
+;;
+
+let%expect_test "Eval huge expression" =
+  try Printf.printf "Success! Sum: %d\n" (sum_n_ones 1_000_000) with
+  | Stack_overflow ->
+    print_endline "Stack overflow!";
+    [%expect.unreachable];
+  [%expect {| Success! Sum: 1000000 |}]
+;;
