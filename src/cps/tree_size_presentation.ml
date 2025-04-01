838de1a3550d1aa8616a828c75fb97bca48e2ef6
Rodion Suvorov <rodion.suvorov.94@mail.ru>
2025-04-01 22:35:05 +0300

    fix: funny thought about cps monad

--- src/cps/tree_size_presentation.ml
+++ src/cps/tree_size_presentation.ml
@@ -20,6 +20,10 @@ let size =
   fun root -> (run[@tailcall]) (fun n -> n) (helper root)
 ;;
 
+(*Короче, оказывается, что данную задачу нельзя решить в том виде, который представлен выше.
+Насколько удалось выяснить, проблема в компиляторе камла версии < 5. Такие дела!
+В конце файла рабочее решение для камла 4.* *)
+
 let test_tree = Node (Node (Node (Leaf, Leaf), Leaf), Node (Leaf, Leaf))
 
 let%expect_test "Simple size evaluating" =
@@ -33,8 +37,40 @@ let rec makek =
   else 
     makek (depth-1) (fun r -> (fun l -> k (Node (l,r))) (if depth = 2 then r else Leaf) ))
 
+    (* широкое дерево - все окей*)
 let%expect_test "Apply function to huge tree" =
   (try print_endline (string_of_int (size (makek 1000000 Fun.id))) with
    | Stack_overflow -> print_endline "Stack overflow!");
   [%expect {| 1000001 |}]
 ;;
+let huge_tree depth =
+  let rec helperk depth k =
+    if depth = 0 then k Leaf else helperk (depth - 1) (fun tree -> k (Node (tree, Leaf)))
+  in
+  helperk depth Fun.id
+;;
+
+(* глубокое дерево - анлак*)
+let%expect_test "Apply function to huge tree" =
+  (try print_endline (string_of_int (size (huge_tree 1000000))) with
+   | Stack_overflow -> print_endline "Stack overflow!");
+  [%expect {| Stack overflow! |}]
+
+(* Спасибо Андрею Зерейновичу *)
+
+let sizerain7 =
+  let rec helper = function
+    | Leaf -> return 0
+    | Node (l, r) ->
+      fun kk ->
+        (helper l) (fun vv ->
+          (fun c k -> (helper r) (fun v -> (fun d -> return (c + d + 1)) v k)) vv kk)
+  in
+  fun root -> (run[@tailcall]) (fun n -> n) (helper root)
+;;
+(*что изменилось? Мы явным образом заинлайнили все функции bind. Содержательно все осталось точно таким же.
+Приколы компилятора.*)
+let%expect_test "Apply function to huge tree" =
+  (try print_endline (string_of_int (sizerain7 (huge_tree 1000000))) with
+   | Stack_overflow -> print_endline "Stack overflow!");
+  [%expect {| 1000000 |}]
