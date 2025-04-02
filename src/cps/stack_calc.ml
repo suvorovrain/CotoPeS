dda7970570746e79f6345645f7216a3661aa56b9
Vyacheslav Kochergin <141725581+VyacheslavIurevich@users.noreply.github.com>
2025-04-02 10:24:45 +0300

    CPS Stack, folds & tree_map (#12)
    * chore: update docs
    
    Signed-off-by: VyacheslavIurevich <vyacheslav.kochergin1@gmail.com>
    
    * chore: add ocamlformat config
    
    Signed-off-by: VyacheslavIurevich <vyacheslav.kochergin1@gmail.com>
    
    * chore: configure build
    
    Signed-off-by: VyacheslavIurevich <vyacheslav.kochergin1@gmail.com>
    
    * chore: configure dune in examples and reformat the code
    
    Signed-off-by: VyacheslavIurevich <vyacheslav.kochergin1@gmail.com>
    
    * refactor: rework examples as i like
    
    Signed-off-by: VyacheslavIurevich <vyacheslav.kochergin1@gmail.com>
    
    * feat: add exercise 1 from presentation
    
    Signed-off-by: VyacheslavIurevich <vyacheslav.kochergin1@gmail.com>
    
    * chore: move examples to src
    
    Signed-off-by: VyacheslavIurevich <vyacheslav.kochergin1@gmail.com>
    
    * chore: remove examples dir
    
    Signed-off-by: VyacheslavIurevich <vyacheslav.kochergin1@gmail.com>
    
    * Revert "chore: remove examples dir"
    
    This reverts commit e399a6f3140713fc0d2c9cd73f754fe82ad48e2e.
    
    * chore: move all examples to src
    
    Signed-off-by: VyacheslavIurevich <vyacheslav.kochergin1@gmail.com>
    
    * feat: add stack calc
    
    Signed-off-by: VyacheslavIurevich <vyacheslav.kochergin1@gmail.com>
    
    * refactor: reformat stack_calc and add comments
    
    Signed-off-by: VyacheslavIurevich <vyacheslav.kochergin1@gmail.com>
    
    * feat: add folds
    
    Signed-off-by: VyacheslavIurevich <vyacheslav.kochergin1@gmail.com>
    
    * refactor: add useful info to folds
    
    Signed-off-by: VyacheslavIurevich <vyacheslav.kochergin1@gmail.com>
    
    * feat: add tree map
    
    Signed-off-by: VyacheslavIurevich <vyacheslav.kochergin1@gmail.com>
    
    * fix: change fold_left_cps1
    
    Signed-off-by: VyacheslavIurevich <vyacheslav.kochergin1@gmail.com>
    
    * test: check stack calc on big expr
    
    Signed-off-by: VyacheslavIurevich <vyacheslav.kochergin1@gmail.com>
    
    * chore: move everything into cps folder
    
    Signed-off-by: VyacheslavIurevich <vyacheslav.kochergin1@gmail.com>
    
    * chore: turn back map from main
    
    Signed-off-by: VyacheslavIurevich <vyacheslav.kochergin1@gmail.com>
    
    * refactor: ocamlformat power
    
    Signed-off-by: VyacheslavIurevich <vyacheslav.kochergin1@gmail.com>
    
    * chore: git rm dune nyehehehehehe
    
    Signed-off-by: VyacheslavIurevich <vyacheslav.kochergin1@gmail.com>
    
    ---------
    
    Signed-off-by: VyacheslavIurevich <vyacheslav.kochergin1@gmail.com>

---
+++ src/cps/stack_calc.ml
@@ -0,0 +1,52 @@
+(* Упражнение 2 с лекции про CPS. Описать API из функций start, fin, push, add, mul, чтобы писать код например
+так:
+start push 1 push 2 add push 3 mul fin
+И выражение будет иметь тип int и вычисляться (в польской нотации, стеком) в 9 *)
+(* Код от Карима *)
+let start f = f [] (* просто передали пустой список, имитирующий стек, в продолжение *)
+let push stack x f = f (x :: stack)
+(* добавили значение к голове списка ("стека"), первый push получит его как аргумент от start *)
+
+let add stack f =
+  match stack with
+  | x :: y :: rest ->
+    f ((x + y) :: rest) (* сняли два элемента со стека, сложили на стек их сумму *)
+  | _ -> failwith "Not enough elements on the stack for addition"
+;;
+
+let mul stack f =
+  match stack with
+  | x :: y :: rest ->
+    f ((x * y) :: rest) (* сняли два элемента со стека, сложили на стек их произведение *)
+  | _ -> failwith "Not enough elements on the stack for multiplication"
+;;
+
+let fin stack =
+  match stack with
+  | [ result ] ->
+    result (* если на стеке лишь один элемент, то он и будет ответом в итоге *)
+  | _ -> failwith "Invalid stack state at the end of computation"
+;;
+
+let%expect_test "Eval expression" =
+  print_int (start push 1 push 2 add push 3 mul fin);
+  [%expect {| 9 |}]
+;;
+
+let sum_n_ones n =
+  let rec build_ops n ops =
+    if n <= 0 then start ops else build_ops (n - 1) (fun stack -> push stack 1 ops)
+  in
+  let rec add_ops count ops =
+    if count <= 0 then ops else add_ops (count - 1) (fun stack -> add stack ops)
+  in
+  build_ops n (add_ops (n - 1) fin)
+;;
+
+let%expect_test "Eval huge expression" =
+  try Printf.printf "Success! Sum: %d\n" (sum_n_ones 1_000_000) with
+  | Stack_overflow ->
+    print_endline "Stack overflow!";
+    [%expect.unreachable];
+    [%expect {| Success! Sum: 1000000 |}]
+;;
--- src/memoi/fib.ml
+++ src/memoi/fib.ml
@@ -1,34 +1,40 @@
-
 (* val memoize : ('a -> 'b) -> 'a -> 'b *)
 let memoize f =
-let table = Hashtbl.create 42 (* size *) in
-(fun key ->
-match Hashtbl.find table key with
-| rez -> rez
-| exception Not_found ->
-let rez = f key in
-Hashtbl.add table key rez;
-rez
-)
+  let table =
+    Hashtbl.create 42
+    (* size *)
+  in
+  fun key ->
+    match Hashtbl.find table key with
+    | rez -> rez
+    | exception Not_found ->
+      let rez = f key in
+      Hashtbl.add table key rez;
+      rez
+;;
+
 (* val memoize2 : (('a -> 'b) -> 'a -> 'b) -> 'a -> 'b *)
 let memoize2 f =
-let h = Hashtbl.create 42 (* size *) in
-let rec g x =
-match Hashtbl.find h x with
-| rez -> rez
-| exception Not_found ->
-let rez = f g x in
-Hashtbl.add h x rez;
-rez
-in
-g
+  let h =
+    Hashtbl.create 42
+    (* size *)
+  in
+  let rec g x =
+    match Hashtbl.find h x with
+    | rez -> rez
+    | exception Not_found ->
+      let rez = f g x in
+      Hashtbl.add h x rez;
+      rez
+  in
+  g
+;;
 
 (* let rec fib n =
   if n<2 then n else fib (n-1) + fib (n-2) ;; *)
-let fib_open self n =
-  if n <= 1 then 1 else self (n - 1) + self (n - 2) ;;
-  let rec fib n = fib_open fib n ;;
-  let fib = memoize fib ;;
-  let fib2 = memoize2 fib_open ;;
-  let res1 = fib 30 (* slow *)
-  let res2 = fib2 30 (* faster *)
+let fib_open self n = if n <= 1 then 1 else self (n - 1) + self (n - 2)
+let rec fib n = fib_open fib n
+let fib = memoize fib
+let fib2 = memoize2 fib_open
+let res1 = fib 30 (* slow *)
+let res2 = fib2 30 (* faster *)
