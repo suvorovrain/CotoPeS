b5fed257d62a15a08f540d0c84cffcc67b427071
Dabzelos <dabzelos@gmail.com>
2025-04-03 22:03:09 +0300

    fixed conflicts + tests

--- src/memoi/fib.ml
+++ src/memoi/fib.ml
@@ -21,21 +21,7 @@ let memoize: ('a -> 'b) -> 'a -> 'b = fun f ->
   in
   new_fun
   ;;
-
-
-let rec fibo n = 
-  if n <= 1 then n 
-  else fibo (n-1) + fibo (n-2)
   
-(*функция для замера для наглядности сам придумал*)
-let time_it f x =
-  let start = Sys.time () in
-  let result = f x in
-  let finish = Sys.time () in
-  Printf.printf "Execution time: %fs\n" (finish -. start);
-  result
-;;
-
 (*принимает функцию которая принимает функцию того же типа('a->'b), и возврашает функцию того же типа*)
 (*это мемоизация для открытых рекурсий вся соль в том что *)
 let memo_rec : (('a -> 'b) -> 'a -> 'b) -> 'a -> 'b =
@@ -55,14 +41,101 @@ let memo_rec : (('a -> 'b) -> 'a -> 'b) -> 'a -> 'b =
   new_fun
 ;;
 
-let fib_open self n = 
-  if n <= 1 then 1 
-  else self (n - 1) + self (n - 2)
+(*функция для замера для наглядности сам придумал поэтому будет работать плохо аххахаахах*)
+let time_it f x =
+  let start = Sys.time () in
+  let _ = f x in
+  let finish = Sys.time () in
+  (finish -. start);
+;;
+
+(*обычная реализация*)
+let rec fibo n = if n <= 1 then n else fibo (n-1) + fibo (n-2)
+
+(* открытая рекурсия*)
+let fib_open self n = if n <= 1 then 1 else self (n - 1) + self (n - 2)
+
+(* мемоизация открытой рекурсии - должно работать лучше всего*)
+let memo_rec_fib = memo_rec fib_open
+
+(*обычная мемоизация*)
+let mem_fib = memoize fibo
+
+(* видим что реализация мемоизированного фибоначи выполняется больше чем за 10 секунд первый раз*)
+(*из теста видно что первый раз функция раотает долго (у меня 45секунд первый запуск и 0.00001 второй)*)
+(* но трейтий запуск от числа 49 все равно займет порядка 45 секунд тк результат не был мемоизирован, мемоизированно было только вычисление от 50*)
+let%expect_test "Compare execution time of stupid memoized fibo" =
+  let time = time_it mem_fib 50 in
+  let reference_time = 10.0 in  (* Максимально допустимое время *)
+  Printf.printf "First execution time: %f (reference: %f)\n" time reference_time;
+  if time > reference_time then
+    Printf.printf "TOO SLOW! execution took more then %.2f)\n"  reference_time
+  else
+    Printf.printf "Acceptable time: execution took less then <= %.2f)\n" reference_time;
+  
+  let time2 = time_it mem_fib 50 in
+  let reference_time_2 = 1.0 in
+  Printf.printf "Second execution time: %f (reference: %f)\n" time2 reference_time_2;
+  if time2 > reference_time_2 then
+    Printf.printf "TOO SLOW! execution took more then %.2f)\n"  reference_time_2
+  else
+    Printf.printf "Acceptable time: execution took less then <= %.2f)\n" reference_time_2;
+  
+  let time3 = time_it mem_fib 49 in
+  let reference_time_3 = 1.0 in
+  Printf.printf "Third execution time: %f (reference: %f)\n" time3 reference_time_3;
+  if time3 > reference_time_3 then
+    Printf.printf "TOO SLOW! execution took more then %.2f)\n"  reference_time_3
+  else
+    Printf.printf "Acceptable time: execution took less then <= %.2f)\n" reference_time_3;  
+  
+  [%expect {|
+    First execution time: 43.579227 (reference: 10.000000)
+    TOO SLOW! execution took more then 10.00)
+    Second execution time: 0.000002 (reference: 1.000000)
+    Acceptable time: execution took less then <= 1.00)
+    Third execution time: 26.729995 (reference: 1.000000)
+    TOO SLOW! execution took more then 1.00) |}]
+;;
 
 
-let fib_opt = memo_rec fib_open
+(*во первых можно заметить что первый запуск занял тоже довольно быстро не 45 секунд а 1 сколько то там тысячных, потому что фактически, каждый вызов фибоначи считает две фунции
+каждый следующий снова две и снова две и снова две, но благодаря такой мемоизации мы посчитали ТОЛЬКО ОДНУ ветку дерева, сохранили все результаты а дальеш толко к мапе обращались!!!!*)
+let%expect_test "Compare execution time of stupid memoized fibo" =
+  let time = time_it memo_rec_fib 50 in
+  let reference_time = 10.0 in  (* Максимально допустимое время *)
+  Printf.printf "First execution time: %f (reference: %f)\n" time reference_time;
+  if time > reference_time then
+    Printf.printf "TOO SLOW! execution took more then %.2f)\n"  reference_time
+  else
+    Printf.printf "Acceptable time: execution took less then <= %.2f)\n" reference_time;
+  
+  let time2 = time_it memo_rec_fib 50 in
+  let reference_time_2 = 1.0 in
+  Printf.printf "Second execution time: %f (reference: %f)\n" time2 reference_time_2;
+  if time2 > reference_time then
+    Printf.printf "TOO SLOW! execution took more then %.2f)\n"  reference_time
+  else
+    Printf.printf "Acceptable time: execution took less then <= %.2f)\n" reference_time_2;
+  
+  let time3 = time_it memo_rec_fib 49 in
+  let reference_time_3 = 1.0 in
+  Printf.printf "Third execution time: %f (reference: %f)\n" time3 reference_time_3;
+  if time3 > reference_time_3 then
+    Printf.printf "TOO SLOW! execution took more then %.2f)\n"  reference_time_3
+  else
+    Printf.printf "Acceptable time: execution took less then <= %.2f)\n" reference_time_3;  
+  [%expect {|
+    First execution time: 0.000010 (reference: 10.000000)
+    Acceptable time: execution took less then <= 10.00)
+    Second execution time: 0.000001 (reference: 1.000000)
+    Acceptable time: execution took less then <= 1.00)
+    Third execution time: 0.000001 (reference: 1.000000)
+    Acceptable time: execution took less then <= 1.00) |}]
+;;
 
-let mem_fib = memoize fibo in
+
+
 
 
 
@@ -95,7 +168,6 @@ let () =
 ```
 *)
 
-
 (*факториал*)
 let factorial_open self n = 
   if n <= 1 then 1  
@@ -110,17 +182,17 @@ let sum_open self n =
 
 let memo_sum = memo_rec sum_open
 
-(*проверка числа на простоту*)
+
+(*открытая рекурсия на функцию является ли число простым*)
 let is_prime_open self n =
   if n <= 1 then false
   else if n = 2 then true
   else 
-    let rec check i = 
+    let check i = 
       if i * i > n then true
       else if n mod i = 0 then false
-      else check (i + 1)
+      else self (i + 1)  (* Используем переданную self вместо прямой рекурсии *)
     in
     check 2
 
 let memo_is_prime = memo_rec is_prime_open
-
